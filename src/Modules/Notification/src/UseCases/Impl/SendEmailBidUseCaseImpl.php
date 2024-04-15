b7b99b14e74ea3f2ecbf3f5d718ead669b394118
d.sarkulin <sarkulindamir@gmail.com>
2024-04-15 10:52:19 +0500

    Edited send email to and added copy

--- src/Modules/Notification/src/UseCases/Impl/SendEmailBidUseCaseImpl.php
+++ src/Modules/Notification/src/UseCases/Impl/SendEmailBidUseCaseImpl.php
@@ -11,9 +11,10 @@ use Innovesta\Notification\UseCases\SendEmailBidUseCase;
 
 final class SendEmailBidUseCaseImpl implements SendEmailBidUseCase
 {
-
     public function handle(BidEntity $bidEntity): void
     {
-        Mail::to(config('notification.base.send_to_email'))->send(new BidEmail($bidEntity));
+        Mail::to(config("notification.base.send_to_email"))
+            ->cc(["info@innovesta.kz", "dkilkeev@gmail.com"])
+            ->send(new BidEmail($bidEntity));
     }
 }
