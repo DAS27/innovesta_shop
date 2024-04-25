6063ea1dc6b04455b3c1e36444ae00a1fa87ff8b
Damir <sarkulindamir@gmail.com>
2024-04-25 15:57:07 +0500

    * HOTFIX

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Cookie;
 
 final class SetLocale
 {
@@ -20,6 +21,12 @@ final class SetLocale
         }
 
         app()->setLocale(config('app.locale'));
+        Cookie::queue(Cookie::make(
+            name: 'locale',
+            value: config('app.locale'),
+            minutes: 10080,
+            httpOnly: false
+        ));
 
         return $next($request);
     }
